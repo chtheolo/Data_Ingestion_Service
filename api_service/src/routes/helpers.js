b95364d993f3ca5a7a39b287baec4aeddd29795c
chtheolo <chtheolo@gmail.com>
2021-10-09 13:42:57 +0300

    Add hardcoded UPSERT funtion.

--- api_service/src/routes/helpers.js
+++ api_service/src/routes/helpers.js
@@ -34,3 +34,29 @@ exports.update = async function(source, endpoint) {
         })
     });
 }
+
+/**
+ * 
+ * @param {Object} source [description] 
+ * @returns {[type]}        [description]
+ */
+exports.upsert = async function(source) {
+    return new Promise((resolve, reject) => {
+        console.log(source);
+        if (!source.sensor_id || !source.threshold_max_value || !source.threshold_min_value) {
+            reject('Not valid body parameters.');
+        }
+
+        let text = `INSERT INTO thresholds (threshold_max_value, threshold_min_value, sensor_id) VALUES (${source.threshold_max_value}, ${source.threshold_min_value}, ${source.sensor_id}) ON CONFLICT(sensor_id) DO UPDATE SET threshold_max_value=EXCLUDED.threshold_min_value, threshold_min_value=EXCLUDED.threshold_min_value;`
+        console.log(text);
+        db.getClient(
+            text,
+            [], 
+            (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(result);
+        });
+    });
+}
