750682b7494a88297717e08ea40ad5bb93945938
chtheolo <chtheolo@gmail.com>
2021-10-09 13:32:00 +0300

    Run only postgresql container in detached mode. Service run in foreground to print logs.

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,3 @@
 # /bin/sh
 
-sudo docker-compose -f infrastructure.yml up -d && sudo docker-compose -f service.yml up --build -d
+sudo docker-compose -f infrastructure.yml up -d && sudo docker-compose -f service.yml up --build
