0fa3c265d9fcce6a232dfdbfc8b9a5225e28689f
chtheolo <chtheolo@gmail.com>
2021-10-09 13:41:09 +0300

    Add UPSERT function for thresholds.

--- api_service/src/routes/thresholds/index.js
+++ api_service/src/routes/thresholds/index.js
@@ -3,16 +3,23 @@ const crud = require('../helpers');
 
 const endpoint = '/thresholds';
 
-exports.get = function(req, res) {
-    (async ()=> {
-        try {
-            let rows = await crud.fetch(req.query, endpoint);
-            return res.status(200).json(rows);
-        }
-        catch(error) {
-            console.log(error);
-            return res.status(404).send(error);
-        }
-    })();
+exports.get = async function(req, res) {
+    try {
+        let rows = await crud.fetch(req.query, endpoint);
+        return res.status(200).json(rows);
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(404).send(error);
+    }
 }
 
+exports.put = async function(req, res) {
+    try {
+        let rows = await crud.upsert(req.body);
+        console.log(rows);
+        return res.status(200).send('ok');
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+}
