d108aa2030e311eb668f795149b5237e24250cba
chtheolo <chtheolo@gmail.com>
2021-10-09 13:45:54 +0300

    Refactor.

--- api_service/src/routes/thresholds/index.js
+++ api_service/src/routes/thresholds/index.js
@@ -17,7 +17,6 @@ exports.get = async function(req, res) {
 exports.put = async function(req, res) {
     try {
         let rows = await crud.upsert(req.body);
-        console.log(rows);
         return res.status(200).send('ok');
     } catch (error) {
         return res.status(400).send(error);
