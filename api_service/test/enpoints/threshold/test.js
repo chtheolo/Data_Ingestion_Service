550bdf9398f89de7b7808f18a2c2aa8a3ce8a006
chtheolo <chtheolo@gmail.com>
2021-10-10 23:26:00 +0300

    Add tests with Mocha.

---
+++ api_service/test/enpoints/threshold/test.js
@@ -0,0 +1,57 @@
+const request = require('supertest');
+const assert = require('assert');
+const expect = require('expect');
+const server = require('./../../../src/server');
+
+describe(`Testing GET - '/threshold'`, ()=> {
+    it('Respond with valid HTTP status codes and message.', (done)=> {
+        request(server)
+            .get('/thresholds?sensor_id=1')
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                expect(res.status).toBe(200);
+                done();
+            });
+    });
+});
+
+describe(`Testing PUT - '/threshold'`, ()=> {
+    it('Respond with valid HTTP status codes and message.', (done)=> {
+        const body = {
+            sensor_id: "1",
+            threshold_max_value: 1120.1,
+            threshold_min_value: -1120.1
+        };
+        request(server)
+            .put('/thresholds')
+            .send(body)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                expect(res.status).toBe(200);
+                done();
+            });
+    });
+});
+
+describe(`Testing PUT - '/threshold' with wrong body`, ()=> {
+    it('Respond with valid HTTP status codes and message.', (done)=> {
+        const body = {
+            threshold_max_value: 1120.1,
+            threshold_min_value: -1120.1
+        };
+        request(server)
+            .put('/thresholds')
+            .send(body)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                expect(res.status).toBe(400);
+                done();
+            });
+    });
+});
